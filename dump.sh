0070eaa448d93aa6d33f83c9478def1279fe959a
Ilya Stepanov <dev@ilyastepanov.com>
2015-09-19 12:22:43 -0700

    Log job begin and end time

--- dump.sh
+++ dump.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+echo "Job started: $(date)"
+
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/dump/$PREFIX-$DATE.sql"
 
 pg_dump -h db -U "$PGUSER" -f "$FILE"
 gzip "$FILE"
+
+echo "Job finished: $(date)"
