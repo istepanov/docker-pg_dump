9dd16e3440b305194445d8fa66d99c5a792ec48d
Cristoffer Fairweather <cfairweather@cfairweather.com>
2016-05-16 19:52:37 -0700

    Adding $PGDB  & $PGHOST
    Adding $PGDB so users can opt to backup another database.

--- dump.sh
+++ dump.sh
@@ -7,7 +7,7 @@ echo "Job started: $(date)"
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/dump/$PREFIX-$DATE.sql"
 
-pg_dump -h db -U "$PGUSER" -f "$FILE"
+pg_dump -h "$PGHOST" -U "$PGUSER" -f "$FILE" -d "$PGDB"
 gzip "$FILE"
 
 if [ ! -z "$DELETE_OLDER_THAN" ]; then
